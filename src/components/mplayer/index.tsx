f3be6f1bce2b6195c884f1e2ec359ee1b7b27e98
baibing <baibing@blacklake.cn>
2020-04-26 18:25:28 +0800

    加入设置按钮

--- src/components/mplayer/index.tsx
+++ src/components/mplayer/index.tsx
@@ -17,17 +17,60 @@ interface PlayerProps {
   width?: number;
 }
 
+interface VideoInfoProps {
+  current: number;
+  duration: number;
+}
+
 function Player({ src, width }: PlayerProps, ref: any) {
   const videoRef = useRef(null);
   const [paused, setPaused] = useState(true);
-  const [progress, setProgress] = useState(0);
+  const [showSetting, setShowSetting] = useState(true);
+  const [menus, setMenus] = useState([
+    {
+      title: "速度",
+      value: 1,
+      children: [
+        {
+          title: "0.25x",
+          value: 0.25,
+        },
+        {
+          title: "0.5x",
+          value: 0.5,
+        },
+        {
+          title: "0.75x",
+          value: 0.75,
+        },
+        {
+          title: "1x",
+          value: 1,
+        },
+        {
+          title: "1.25x",
+          value: 1.25,
+        },
+        {
+          title: "1.5x",
+          value: 1.5,
+        },
+        {
+          title: "2x",
+          value: 2,
+        },
+      ],
+    },
+  ]);
+  const [videoInfo, setVideoInfo] = useState<VideoInfoProps>({
+    current: 0,
+    duration: 0,
+  });
 
   const onTimeUpdateListener = () => {
     const { currentTime, duration } = videoRef?.current;
     console.log("currentTime: ", currentTime, "duration: ", duration);
-    // this.$refs.timeDisplayCurrentTime.innerHTML = getFormattedTime(currentTime);
-    // this.$refs.timeDisplayDuration.innerHTML = getFormattedTime(duration);
-    setProgress((currentTime / duration) * 100);
+    setVideoInfo({ current: currentTime, duration });
   };
 
   useEffect(() => {
@@ -37,6 +80,7 @@ function Player({ src, width }: PlayerProps, ref: any) {
 
       try {
         await player.load(src);
+        console.log("player.getVariantTracks: ", player?.getVariantTracks());
       } catch (err) {
         console.error("Error code", err.code, "object", err);
       }
@@ -54,15 +98,11 @@ function Player({ src, width }: PlayerProps, ref: any) {
     // get player() {
     //   return controller.current.player;
     // },
-    // get ui() {
-    //   return controller.current.ui;
-    // },
     get video() {
       return videoRef.current;
     },
   }));
 
-  // return <ShakaPlayer autoPlay src={src} width={width} />;
   return (
     <div className={styles.mplayer}>
       <video ref={videoRef} className={styles.video} />
@@ -81,11 +121,37 @@ function Player({ src, width }: PlayerProps, ref: any) {
               setPaused(!paused);
             }}
           />
+
+          <div className={styles.timeDisplay}>
+            <span>{getFormattedTime(videoInfo?.current)}</span>
+            <span>{` / `}</span>
+            <span>{getFormattedTime(videoInfo?.duration)}</span>
+          </div>
+        </div>
+
+        <div className={styles.right}>
+          <Icon
+            name="settings"
+            onClick={() => {
+              setShowSetting(!showSetting);
+            }}
+          />
+
+          <div className={styles.playSettings}>
+            {menus.map((menu) => (
+              <div className={styles.menu}>{menu.title}</div>
+            ))}
+          </div>
         </div>
       </div>
 
       <div className={styles.progressBar}>
-        <span className={styles.played} style={{ width: `${progress}%` }} />
+        <span
+          className={styles.played}
+          style={{
+            width: `${(videoInfo?.current / videoInfo?.duration) * 100}%`,
+          }}
+        />
         <span className={styles.buffered} />
       </div>
     </div>
